164e2c27dd9c88386c56bf8d53834143f907e350
James Karnes <karnes.james@gmail.com>
2021-12-03 16:40:19 -0400

    testing heroku error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 const appName = 'real-estate'
 
+const dbUrl = process.env.DB_URL || `${process.env.DB}/${appName}`
 
-mongoose.connect(`${process.env.DB}/${appName}`, {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 
